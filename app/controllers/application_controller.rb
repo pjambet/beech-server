b060d943c3622a9aaf7110cc41c91585c854edf2
pjambet <pierre.jambet@gmail.com>
2012-11-05 23:58:04 +0100

    Add first views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(user)
+    user_path user
+  end
+
+  def after_sign_up_path_for(user)
+    user_path user
+  end
+
 end
