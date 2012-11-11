c0291fa79f6a78b2c9a9643df9f5f6925604e3ed
pjambet <pierre.jambet@gmail.com>
2012-11-12 00:46:58 +0100

    Add ams

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,5 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email
+  has_many :beers
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 BeerServer::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   resources :users, only: :show
 
