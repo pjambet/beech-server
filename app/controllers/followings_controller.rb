b5a4cefb1bf6bc2903a1ee07f3a4fd1d3454cada
pjambet <pierre.jambet@gmail.com>
2012-11-24 21:43:48 +0100

    Update rails

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,4 +1,9 @@
 class FollowingsController < ApplicationController
+
+  def index
+    current_user.following_users
+  end
+
   def create
     follower = User.new params[:followee]
     unless current_user.following_users.include? follower
