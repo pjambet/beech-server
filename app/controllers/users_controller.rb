f98a8c91977df74220c1c28fd1ae94152c348e4e
pjambet <pierre.jambet@gmail.com>
2012-11-25 13:24:15 +0100

    Add carrierwave and default image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.except(current_user).limit(10)
+    render json: @users
+  end
+
   def show
     @user = User.find(params[:id])
     render json: @user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BeerServer::Application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
 
-  resources :users, only: :show do
+  resources :users, only: [ :show, :index ] do
     resources :checks, only: [ :index, :create ]
     resources :followings, only: [ :index, :create ]
     resources :followers, only: :index
