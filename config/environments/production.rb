fad3f449a64300ee9c48bae9f96b38e96314df37
pjambet <pierre.jambet@gmail.com>
2012-11-27 02:58:05 +0100

    Add dalli gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,15 @@ BeerServer::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+
+  config.action_dispatch.rack_cache = {
+    :metastore    => Dalli::Client.new,
+    :entitystore  => 'file:tmp/cache/rack/body',
+    :allow_reload => false
+  }
+
+  config.static_cache_control = "public, max-age=2592000"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
