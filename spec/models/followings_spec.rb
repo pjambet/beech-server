23ac3bb9831d548bf557ed2143fd6d7fabecc27c
pjambet <pierre.jambet@gmail.com>
2012-12-07 20:18:57 +0100

    Add missing specs for Event & User

--- spec/models/followings_spec.rb
+++ spec/models/followings_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Following do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :follower }
+  it { should belong_to :followee }
+
+  it { should validate_presence_of :follower }
+  it { should validate_presence_of :followee }
+
+  it { should have_db_index [:follower_id, :followee_id]}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,19 @@ require 'spec_helper'
 
 describe User do
   it { should have_many :awards }
-  it { should have_many( :badges ).through( :awards ) }
+  it { should have_many(:badges).through(:awards) }
   it { should have_many :checks }
-  it { should have_many( :beers ).through( :checks ) }
+  it { should have_many(:beers).through(:checks) }
+  it { should have_many(:events) }
+  it { should have_many(:followings) }
+  it { should have_many(:following_users).through(:followings) }
+  it { should have_many(:followers) }
+  it { should have_many(:follower_users).through(:followers) }
+
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :nickname }
+
+  describe '.except' do
+    # TODO
+  end
 end
