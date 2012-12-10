de75081fd72c097115b7751862067560601c1555
pjambet <pierre.jambet@gmail.com>
2012-12-10 22:04:41 +0100

    Use simple quotes for non extrapolated strings

--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -2,12 +2,12 @@ class Api::ProfileController < Api::ApplicationController
 
   def index
     @user = current_user
-    render json: @user, serializer: MyUserSerializer, root: "my_profile"
+    render json: @user, serializer: MyUserSerializer, root: 'profile'
   end
 
   def show
     @user = User.find params[:id]
-    render json: @user, serializer: MyUserSerializer, root: "my_profile"
+    render json: @user, serializer: MyUserSerializer, root: 'profile'
   end
 end
 
