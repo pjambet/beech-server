b5d26b2c266a2cee10ec68fb147ef5ab1837fd53
pjambet <pierre.jambet@gmail.com>
2012-12-26 10:45:17 +0100

    Add basic error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,28 @@ class ApplicationController < ActionController::Base
       User.find(1)
     end
   end
+
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: (lambda do |exception|
+      # TODO notify exception (airbrake or honeybadger)
+      render_error 500, exception
+    end)
+    rescue_from CanCan::AccessDenied,
+              with: lambda { |exception| render_error 401, exception }
+
+    rescue_from ActionController::RoutingError,
+                ActionController::UnknownController,
+                ::AbstractController::ActionNotFound,
+                ActiveRecord::RecordNotFound,
+                with: lambda { |exception| render_error 404, exception }
+  end
+
+  def render_error(status, exception)
+    respond_to do |format|
+      format.html { render template: "errors/error_#{status}", layout: 'layouts/errors', status: status }
+      format.all { render nothing: true, status: status }
+    end
+  end
+
+
 end
