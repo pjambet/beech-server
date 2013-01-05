34feb6e1d812b032779bde09762402832e388c6d
pjambet <pierre.jambet@gmail.com>
2013-01-05 17:57:54 +0100

    use a gitignored file for secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,16 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BeerServer::Application.config.secret_token = '271a51cfdcdeaa9d4b3b69758194409b7d4684bad1fa5a16ec57c2a578c8fc84a3216cb2621d827d9ad9a79b3669c23eb2550fc647ea7c5a669d48225d613897'
+#
+# old, probably compromised, open on GitHub
+# BeerServer::Application.config.secret_token = '271a51cfdcdeaa9d4b3b69758194409b7d4684bad1fa5a16ec57c2a578c8fc84a3216cb2621d827d9ad9a79b3669c23eb2550fc647ea7c5a669d48225d613897'
+begin
+    token_file = Rails.root.to_s + "/secret_token"
+    to_load = open(token_file).read
+    BeerServer::Application.configure do
+        config.secret_token = to_load
+    end
+rescue LoadError, Errno::ENOENT => e
+    raise "Secret token couldn't be loaded! Error: #{e}"
+end
+
