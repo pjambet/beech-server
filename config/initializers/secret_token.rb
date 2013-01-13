103361fb1592b2ce2e05710ed69df7d030120e43
pjambet <pierre.jambet@gmail.com>
2013-01-13 13:22:08 +0100

    Use ENV variable in production environment to read secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,13 +7,12 @@
 #
 # old, probably compromised, open on GitHub
 # BeerServer::Application.config.secret_token = '271a51cfdcdeaa9d4b3b69758194409b7d4684bad1fa5a16ec57c2a578c8fc84a3216cb2621d827d9ad9a79b3669c23eb2550fc647ea7c5a669d48225d613897'
-begin
-    token_file = Rails.root.to_s + "/secret_token"
-    to_load = open(token_file).read
-    BeerServer::Application.configure do
-        config.secret_token = to_load
-    end
-rescue LoadError, Errno::ENOENT => e
-    raise "Secret token couldn't be loaded! Error: #{e}"
+if secret_token = ENV['SECRET_TOKEN']
+  # production env should define the secret token
+  config.secret_token = secret_token
+else
+  # used for development
+  config.secret_token = '9337470c9fdff55ca07e67299c279540487a023bdff5c1b46f01608fdc1677d1e41e407d44ad3c7ecc37b44bb414c8b04f9d1a1c214bbc070fa4f180e57221df'
 end
+
 
