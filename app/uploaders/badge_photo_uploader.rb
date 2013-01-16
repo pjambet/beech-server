c97c673e76f97ac28fef5328d3fa478019d42981
pjambet <pierre.jambet@gmail.com>
2013-01-16 21:39:21 +0100

    Add new uploader for badge images

---
+++ app/uploaders/badge_photo_uploader.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+class BadgePhotoUploader < BaseUploader
+
+  DEFAULT_BADGE_NAME = "default-badge"
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
+    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    asset_path "#{[version_name, DEFAULT_BADGE_NAME].compact.join('_')}.png"
+  end
+
+  process resize_to_fill: [100, 100]
+
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
+end
+
---
+++ app/uploaders/base_uploader.rb
@@ -0,0 +1,20 @@
+class BaseUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
