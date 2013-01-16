36d14880e0113172a9b57b271c0e20161eea00e8
pjambet <pierre.jambet@gmail.com>
2013-01-16 22:57:37 +0100

    When page is nil are invalid (<=0) then consider page as 1

--- app/models/concerns/pageable.rb
+++ app/models/concerns/pageable.rb
@@ -5,7 +5,7 @@ module Pageable
   module ClassMethods
     def acts_as_pageable(opts = {})
       scope :paginate, ->(page, opts = {}) do
-        page ||= 1
+        page = (page.nil? || page.to_i <= 0 ) ? 1 : page
         limit(per_page).offset((page.to_i - 1) * per_page )
       end
 
