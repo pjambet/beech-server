474a5b87c929c724fe161958bd94e105eeabe341
pjambet <pierre.jambet@gmail.com>
2013-01-17 01:38:00 +0100

    temporary remove feed pagination

--- app/controllers/api/feed_controller.rb
+++ app/controllers/api/feed_controller.rb
@@ -3,7 +3,8 @@ class Api::FeedController < Api::ApplicationController
   def index
     @events = Event.for_users(current_user.self_and_following_users)
     @events = @events.after(params[:after]) if params[:after].present?
-    @events = @events.paginate params[:page]
+    # TEMPORARY
+    # @events = @events.paginate params[:page]
 
     render json: @events
   end
