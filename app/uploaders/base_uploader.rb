fe4dbde242d1612728e36495ea41cea2453c5ce9
pjambet <pierre.jambet@gmail.com>
2013-01-17 01:21:48 +0100

    Use S3

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -3,10 +3,6 @@ class BaseUploader < CarrierWave::Uploader::Base
   include Sprockets::Helpers::RailsHelper
   include Sprockets::Helpers::IsolatedHelper
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+      :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = ENV['S3_BUCKET']                     # required
+    config.fog_public     = false                                   # optional, defaults to true
+  elsif Rails.env.development?
+    config.storage = :file
+  end
+end
