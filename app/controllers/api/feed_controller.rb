6f0cee0463e8ac364633d164eab48a9fb45040a0
pjambet <pierre.jambet@gmail.com>
2013-02-03 01:29:36 +0100

    Add before scope which acts as a pagination

--- app/controllers/api/feed_controller.rb
+++ app/controllers/api/feed_controller.rb
@@ -3,8 +3,8 @@ class Api::FeedController < Api::ApplicationController
   def index
     @events = Event.for_users(current_user.self_and_following_users)
     @events = @events.after(params[:after]) if params[:after].present?
-    # TEMPORARY
-    # @events = @events.paginate params[:page]
+    @events = @events.before(params[:before]) if params[:before].present?
+    @events = @events.paginate params[:page]
 
     render json: @events
   end
