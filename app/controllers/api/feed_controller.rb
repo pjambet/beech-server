f593d56e2e881aa817785b15990485b732fc8298
pjambet <pierre.jambet@gmail.com>
2013-02-03 15:01:55 +0100

    Allow loading of other's users feed

--- app/controllers/api/feed_controller.rb
+++ app/controllers/api/feed_controller.rb
@@ -1,7 +1,9 @@
 class Api::FeedController < Api::ApplicationController
+  include UserLoader
+  load_user
 
   def index
-    @events = Event.for_users(current_user.self_and_following_users)
+    @events = Event.for_users(@user.self_and_following_users)
     @events = @events.after(params[:after]) if params[:after].present?
     @events = @events.before(params[:before]) if params[:before].present?
     @events = @events.paginate params[:page]
