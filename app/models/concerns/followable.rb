fd2baef71098ebb5cbf40c4da564e295b8230abd
pjambet <pierre.jambet@gmail.com>
2013-02-10 19:49:13 +0100

    Add missing dependent destroy so Following instances are deleted when one of the Following or the Followee is destroyed

--- app/models/concerns/followable.rb
+++ app/models/concerns/followable.rb
@@ -3,9 +3,9 @@ module Followable
   extend ActiveSupport::Concern
 
   included do
-    has_many :followings, foreign_key: :follower_id, class_name: 'Following'
+    has_many :followings, foreign_key: :follower_id, class_name: 'Following', dependent: :destroy
     has_many :following_users, through: :followings, source: :followee
-    has_many :followers, foreign_key: :followee_id, class_name: 'Following'
+    has_many :followers, foreign_key: :followee_id, class_name: 'Following', dependent: :destroy
     has_many :follower_users, through: :followers, source: :follower
   end
 end
