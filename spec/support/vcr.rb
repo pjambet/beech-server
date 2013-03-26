c779cbf0ff93ac10c03bab4d7d08ef7808d56852
pjambet <pierre.jambet@gmail.com>
2013-03-25 21:31:12 -0400

    Prevent VCR from blocking coveralls request
    This has been fixed but not yet released

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,4 +2,8 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+
+  config.ignore_request do |request|
+    request.uri =~ /coveralls/
+  end
 end
