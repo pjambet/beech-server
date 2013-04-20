9c718871d2ccd141bc91d9e243623bc0a993d24d
pjambet <pierre.jambet@gmail.com>
2013-04-20 19:10:26 -0400

    Returb api key upon authentication

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,10 +5,17 @@ class UserSerializer < ActiveModel::Serializer
 
   embed :ids, include: true
 
-  attributes :id, :email, :nickname, :avatar_url, :already_following
+  attributes :id, :email, :nickname, :avatar_url, :already_following, :authentication_token
 
   def avatar_url
     image_url
+  end
+
+  def include_authentication_token?
+    # If action was successful signed in
+    path_segment = @options[:url_options][:_path_segments]
+    return false unless path_segment
+    @options[:status] == 201 && path_segment[:action] == 'create' && path_segment[:controller] == 'users/sessions'
   end
 end
 
