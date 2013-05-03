3a867e96d5937d245a03f7993c79e9461913ba2f
pjambet <pierre.jambet@gmail.com>
2013-05-02 22:40:41 -0400

    Return country for beers

--- app/serializers/beer_serializer.rb
+++ app/serializers/beer_serializer.rb
@@ -1,5 +1,5 @@
 class BeerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :font_color, :background_color
+  attributes :id, :name, :font_color, :background_color, :country
 
   embed :ids, include: true
   has_one :beer_color
