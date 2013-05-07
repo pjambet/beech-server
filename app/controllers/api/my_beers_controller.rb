fad166bee56aadfcbcdcd246df3713bc31f952ef
pjambet <pierre.jambet@gmail.com>
2013-05-06 21:59:13 -0400

    Fix small error resulting from pagination deletion

--- app/controllers/api/my_beers_controller.rb
+++ app/controllers/api/my_beers_controller.rb
@@ -5,7 +5,7 @@ class Api::MyBeersController < Api::ApplicationController
   api :GET, "/my/beers", "List my beers"
   api :GET, "/users/:user_id/beers", "List my beers"
   def index
-    @beers = @user.beers.ordered params[:page]
+    @beers = @user.beers.ordered
     render json: @beers, each_serializer: MyBeersSerializer, root: 'beers'
   end
 end
