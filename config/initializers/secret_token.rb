3a38fc884cee445c970802326f4b6f2ae2e798e6
pjambet <pierre.jambet@gmail.com>
2013-05-11 17:30:16 -0400

    Add secret_key_base for rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,12 +7,14 @@
 #
 # old, probably compromised, open on GitHub
 # BeerServer::Application.config.secret_token = '271a51cfdcdeaa9d4b3b69758194409b7d4684bad1fa5a16ec57c2a578c8fc84a3216cb2621d827d9ad9a79b3669c23eb2550fc647ea7c5a669d48225d613897'
-if secret_token = ENV['SECRET_TOKEN']
+if secret_token = ENV['SECRET_TOKEN'] && secret_key_base = ENV['SECRET_KEY_BASE']
   # production env should define the secret token
   BeerServer::Application.config.secret_token = secret_token
+  BeerServer::Application.config.secret_key_base = secret_key_base
 else
   # used for development
   BeerServer::Application.config.secret_token = '9337470c9fdff55ca07e67299c279540487a023bdff5c1b46f01608fdc1677d1e41e407d44ad3c7ecc37b44bb414c8b04f9d1a1c214bbc070fa4f180e57221df'
+  BeerServer::Application.config.secret_key_base = '9337470c9fdff55ca07e67299c279540487a023bdff5c1b46f01608fdc1677d1e41e407d44ad3c7ecc37b44bb414c8b04f9d1a1c214bbc070fa4f180e57221df'
 end
 
 
