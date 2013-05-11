eca0e54fa420f3ff6f0cb5697e6cdac088c31b4c
pjambet <pierre.jambet@gmail.com>
2013-05-11 14:48:36 -0400

    Add rack_cache config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,6 +11,14 @@ BeerServer::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
 
+  config.action_dispatch.rack_cache = {
+    metastore: Dalli::Client.new,
+    entitystore: 'file:tmp/cache/rack/body',
+    allow_reload: false
+  }
+
+  config.eager_load = true
+
   config.static_cache_control = "public, max-age=2592000"
 
   # Compress JavaScripts and CSS
